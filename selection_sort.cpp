540ea433da0a69a96c9e53f5b5c6f1113da6ce06
Akhilesh Jaguri <akhileshjaguri3402@gmail.com>
2026-04-13 19:16:37 +0530

    Add selection sort algorithm with array input

---
+++ selection_sort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for(int i = 0; i < n-1; i++) {
+        int minIdx = i;
+        for(int j = i+1; j < n; j++) {
+            if(arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        int temp = arr[minIdx];
+        arr[minIdx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void display(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "Before sorting: ";
+    display(arr, n);
+    selectionSort(arr, n);
+    cout << "After sorting: ";
+    display(arr, n);
+    return 0;
+}
